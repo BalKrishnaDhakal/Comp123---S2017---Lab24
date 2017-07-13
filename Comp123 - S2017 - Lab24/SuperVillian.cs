07009c197702031629bcb63d00913152f17cd405
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-07-13 18:13:34 -0400

    Added SuperVillian class

---
+++ Comp123 - S2017 - Lab24/SuperVillian.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Comp123___S2017___Lab24
+{
+    public class SuperVillian
+    {
+        // PRIVATE INSTANCE VARIABLES
+        private int _karma;
+        // PUBLIC PROPERTIES
+        public int Karma
+        {
+            get
+            {
+                return this._karma;
+
+            }
+            set
+            {
+                this._karma = value;
+
+            }
+        }
+
+        // CONSTRUCTORS
+        /// <summary>
+        /// This is the main constructor for the SuperHuman class
+        /// It takes one argument - name(string)
+        /// </summary>
+        /// <param name="name"></param>
+        public SuperHero(string name, int karma)
+            : base(name)
+        {
+            this.Karma = karma;
+
+        }
+        // PRIVATE METHODS
+        // PUBLIC METHODS
+    }
+}
