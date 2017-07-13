b0d884c9ec7b59ca7cbee83c12565cda5b5c3974
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-07-13 15:18:30 -0400

    Added SuperHuman Class

---
+++ Comp123 - S2017 - Lab24/SuperHuman.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Name: Bal Krishna Dhakal
+ * Date: July 13, 2017
+ * Description: This is the Super Human Class that extends the Human abstract class 
+ * Version: 0.1 - Created the Super Human Class
+ */
+namespace Comp123___S2017___Lab24
+{/// <summary>
+/// This is the SupeHuman Class
+/// </summary>
+   public class SuperHuman : Human
+    {
+        // PRIVATE INSTANCE VARIABLES
+        // PUBLIC PROPERTIES
+        // CONSTRUCTORS
+        public SuperHuman(string name)
+            : base(name)
+        {
+        
+        }
+        
+
+        }
+        // PRIVATE METHODS
+        // PUBLIC METHODS
+    }
+}
