f492f3a76306b50b0f3be92b47819c4587b93d95
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-07-13 14:49:37 -0400

    Modified Program.cs

--- Comp123 - S2017 - Lab24/Program.cs
+++ Comp123 - S2017 - Lab24/Program.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/* Name: Bal Krishna Dhakal
+ * Date: July 13, 2017
+ * Description: Lab 24
+ * Version: 0.1 - Created the Program Class
+ */
 namespace Comp123___S2017___Lab24
 {
     class Program
